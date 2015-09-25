53bbe92ea8352f829a90239c08263ca132dcfc73
cojoco <cojoco@users.noreply.github.com>
2015-09-25 13:17:36 -0600

    Updated javascript

--- source.js
+++ source.js
@@ -1,1 +1,11 @@
-Placeholder!
+
+
+var myurl = "http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?q=" + word + "&api-key=d8a310ff4bda32f06c0a5b2634ade6fc:16:73061887";
+$.ajax({
+  url: myurl,
+  crossDomain: true,
+  dataType: "jsonp",
+  success: function(data) {
+    console.log(data);
+  }
+})
