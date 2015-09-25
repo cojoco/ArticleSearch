b708cc6a16d1e2f1d53dc19de6729ce6c311e5f6
cojoco <cojoco@users.noreply.github.com>
2015-09-25 14:16:39 -0600

    Fixed source.js

--- source.js
+++ source.js
@@ -1,6 +1,7 @@
+
+$("")
 var word = $( "#search" ).value;
 console.log(word);
-
 var myurl = "http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?q=" + word + "&api-key=d8a310ff4bda32f06c0a5b2634ade6fc:16:73061887";
 $.ajax({
   url: myurl,
@@ -9,16 +10,15 @@ $.ajax({
   jsonp: 'callback',
   jsonpCallback: "svc_search_v2_articlesearch",
   success: function(data) {
-  },
-  error: function(jqxHR, status, errorThrown) {
-    console.log(jqxHR);
-  }
     for (var i = 0; i < 10; i++) {
       var curArticle = "#article" + (i+1);
       var curSnippet = data.response.docs[i].snippet;
       var curUrl = data.response.docs[i].web_url;
       $(curArticle).html(curUrl + "<br>" + curSnippet);
     }
+  },
+  error: function(jqxHR, status, errorThrown) {
+    console.log(jqxHR);
   }
 
 });
