ff808983f0ccbbf35dcbde426bb7ec88ed68b67f
cojoco <cojoco@users.noreply.github.com>
2015-09-25 15:30:01 -0600

    changed url to title

--- source.js
+++ source.js
@@ -4,6 +4,7 @@ $(function() {
 
     e.preventDefault();
     var word = $("#search").val();
+    console.log(word);
     var myurl = "http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?q=" + word + "&api-key=d8a310ff4bda32f06c0a5b2634ade6fc:16:73061887";
     $.ajax({
       url: myurl,
@@ -15,7 +16,7 @@ $(function() {
         for (var i = 0; i < 10; i++) {
           var curArticle = "#article" + (i+1);
           var curSnippet = data.response.docs[i].snippet;
-          var curUrl = data.response.docs[i].web_url;
+          var curUrl = "<a href=" + data.response.docs[i].web_url + ">Read more here!</a>";
           $(curArticle).html(curUrl + "<br>" + curSnippet);
         }
       },
